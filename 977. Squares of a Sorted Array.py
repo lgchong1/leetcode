d315b3c2d3e0da0b74f628456e99051fc3e86cb0
Lawrence Chong <lawrence88chong@gmail.com>
2020-04-01 21:03:21 -0700

    added notes to 977

--- 977. Squares of a Sorted Array.py
+++ 977. Squares of a Sorted Array.py
@@ -1,6 +1,9 @@
 # 977. Squares of a Sorted Array
 # by L.Chong 4-1-20
 
+# time : O(NlogN) - Python's sort() is based on timsort, which is O(NlogN)
+# space: O(N)     - sorts in place, no extra space needed.
+
 class Solution:
 	def sortedSquares(self, A:List[int]) -> List[int]:
 		for i in range(len(A)):

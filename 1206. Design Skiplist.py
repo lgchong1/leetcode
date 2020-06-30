b9e4ceff3cc36d9b0d00e19f62b90af35ae8347c
Lawrence Chong <lawrence88chong@gmail.com>
2020-06-30 16:42:46 -0700

    1206, added add2 and print

--- 1206. Design Skiplist.py
+++ 1206. Design Skiplist.py
@@ -13,9 +13,8 @@ class Node(object):
         self.next = next
         self.below = below
         
-        if DEBUG: print("New Node:\n\tval: {}\n\tnext val: {}\n\tbelow.val: {}".format(val,next,below))        
+        if DEBUG: print("New Node:\n\tval: {}\n\tnext val:{}\n\tbelow.val: {}".format(val,next,below))        
             
-
             
 class Skiplist(object):
 
@@ -30,6 +29,10 @@ class Skiplist(object):
         :type target: int
         :rtype: bool
         """
+        result = False
+
+        return result
+        
         
 
     def add(self, num):
@@ -38,6 +41,46 @@ class Skiplist(object):
         :rtype: None
         """
         
+    def add2(self,num):
+        """
+        This is the add to a single linked list
+        """
+        
+        if DEBUG: print("add({})".format(num))   
+
+        newNode = Node(num)
+        
+        if not self.head:
+            if DEBUG: print("\t{} added to head of empty list".format(num))   
+            self.head = newNode
+        else:
+            cur = self.head
+
+            # insert at front
+            if num < cur.val:
+                if DEBUG: print("\t{} added to head of nonempty list".format(num))   
+                self.head = newNode
+                newNode.next = cur
+                return
+
+            while(cur):
+                # end of the list, correct
+                if cur.next is None:
+                    cur.next = newNode
+                    if DEBUG: print("\t{} added at end of list".format(num))   
+                    return
+                # if new value is greater than next value, keep going
+                elif num >= cur.next.val:
+                    cur = cur.next
+                    return
+                # if new value is less than next value, insert
+                elif num < cur.next.val:
+                    newNode.next = cur.next
+                    cur.next = newNode
+                    if DEBUG: print("\t{} added in middle of list".format(num))   
+                    return
+                if DEBUG: print("\tincrement in an unknown spot".format())   
+                cur = cur.next
 
     def erase(self, num):
         """
@@ -49,6 +92,7 @@ class Skiplist(object):
     
     def numHeads(self):
         count = 0
+
         while (self.coinFlip() == HEADS):
             count += 1
         return count
@@ -56,9 +100,45 @@ class Skiplist(object):
     def coinFlip(self):
         rand = int(random.random()*10) % 2
         return rand
-        
+
+    def printList(self):
+        # print entire skiplist
+        pass
+
+    def printList2(self):
+        # print one level of skiplist
+        if DEBUG: print("PrintList2()")   
+        level = []
+    
+        cur = self.head
+
+        while cur:
+            level.append(cur.val)
+            cur = cur.next
+
+        space = ""
+        if DEBUG: space ="\t"
+            
+        if level:
+            print(space + str(level))
+        else:
+            print(space + "Empty Level")
+    
 # Your Skiplist object will be instantiated and called as such:
 # obj = Skiplist()
 # param_1 = obj.search(target)
 # obj.add(num)
 # param_3 = obj.erase(num)
+
+if __name__ == "__main__":
+    skip = Skiplist()
+
+    skip.printList2()
+    skip.add2(20)
+    skip.add2(15)
+    skip.add2(1)
+
+    skip.add2(22)
+    skip.add2(7)
+    skip.add2(0)
+    skip.printList2()

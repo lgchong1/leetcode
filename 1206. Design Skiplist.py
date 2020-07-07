c1ad876df5e73c4ae66bdb517a524490d9178d46
Lawrence Chong <lawrence88chong@gmail.com>
2020-07-07 13:58:37 -0700

    add mostly done

--- 1206. Design Skiplist.py
+++ 1206. Design Skiplist.py
@@ -4,22 +4,20 @@
 import random
 
 DEBUG = True
-HEADS = 1
-TAILS = 0
 
 class Node(object):
-    def __init__(self, val, next=None, below = None):
+    def __init__(self, val=None, next=None, below = None):
         self.val = val
         self.next = next
         self.below = below
         
-        if DEBUG: print("New Node:\n\tval: {}\n\tnext val:{}\n\tbelow.val: {}".format(val,next,below))        
+        if DEBUG: print("Node.__init__({}, {}, {})".format(val,next,below))
             
             
 class Skiplist(object):
 
     def __init__(self):
-        self.head = None
+        self.head = Node()
         self.levels = 1
         
         if DEBUG: print("Skiplist instantiated")   
@@ -33,31 +31,111 @@ class Skiplist(object):
 
         return result
         
-        
-
     def add(self, num):
         """
         :type num: int
         :rtype: None
         """
-
-        if DEBUG: print('add({})'.format(5))
+        if DEBUG: print('add({})'.format(num))
         
-        print('\t' + str(self.__numHeads()))
+        heads = self.__numHeads()
         
+        if DEBUG: print('\tnum levels  : {}'.format(self.levels))
+        if DEBUG: print('\theads rolled: {}'.format(heads))
 
+        if heads > self.levels:
+            if DEBUG: print('\t{} rows added'.format(heads-self.levels))
+            self.__addRowsAbove(heads - self.levels)
+        else:
+            if DEBUG: print('\tno rows added')
+
+        skip = self.levels - heads
+        if DEBUG: print('\twill add {} to the bottom {} levels and will skip top {} levels'.format(num,heads,skip))
+
+        cur = self.head
+        for _ in range(skip):
+            cur = cur.below
+        while cur:
+            self.__addToLevel(cur,num)
+            cur = cur.below
+        
     def erase(self, num):
         """
         :type num: int
         :rtype: bool
         """
-        
+        pass
+    
     #HELPERS
     
-    def __numHeads(self):
-        count = 0
+    def __addRowsAbove(self, times):
+        if DEBUG: print('\t__addRowsAbove({})'.format(times))
 
-        while (self.__coinFlip() == HEADS):
+        for _ in range(times):
+            new = Node(None,None,self.head)
+            self.head = new
+        self.levels += times
+
+    def __addToLevel(self, head, value):
+        """Inserts a new node into a level, returning the new node."""
+
+        if DEBUG: print('\t__addToLevel({})'.format(value))
+
+        cur = head
+        
+        if cur.next == None:
+            self.__insert(cur,value)
+            return cur
+        
+        #cur = cur.next
+
+        while cur:
+            if cur.next == None or \
+                cur.val == value or\
+                cur.next.val > value:
+                self.__insert(cur,value)
+                output = cur
+                break
+            cur = cur.next
+        return output
+             
+    def __insert(self, node, value):
+        """ Inserts a new node after the passed node, returning the new node.
+
+            Given a node and a value, will insert a new node behind the
+            parameter node, assigning that new node's val to value. This function
+            does no comparisons and assumes input to be modified is intended.
+
+            Args:
+                node: a node object after which the new node will be inserted
+                value: the value that the new node will assume
+
+            Returns:
+                A node whose val will be value and whose next will be node.next
+                
+                A visualization:
+
+                    Before __insert()
+
+                        node -> node.next
+
+                    After __insert()
+
+                        node -> new -> node.next
+                                L>val
+
+        """
+        if DEBUG: print('\t__insert({})'.format(value))
+
+        new = Node(value, node.next)
+        node.next = new
+        return new
+
+    def __numHeads(self):
+        """Returns the number of consecutive heads from coin flips (at least 1)."""
+        count = 1
+
+        while (self.__coinFlip() == 1):
             count += 1
         return count
         
@@ -67,14 +145,13 @@ class Skiplist(object):
 
     def printList(self):
         # print entire skiplist
-        if DEBUG: print("PrintList()")   
+        if DEBUG: print("printList()")   
         cur = self.head
 
         while cur:
             self.__printLevel(cur)
             cur = cur.below
         
-
     def __printLevel(self, head):
         # print one level of skiplist
         level = []
@@ -93,8 +170,14 @@ class Skiplist(object):
         else:
             print(space + "Empty Level")
 
+    def countRows(self):
+        count = 1
+        cur = self.head
+        while(cur):
+            cur = cur.below
+            count +=1
+        return count
 
-    
 # Your Skiplist object will be instantiated and called as such:
 # obj = Skiplist()
 # param_1 = obj.search(target)
@@ -103,6 +186,12 @@ class Skiplist(object):
 
 if __name__ == "__main__":
     skip = Skiplist()
-
-    for _ in range(10):
-        skip.add(5)
+    skip.printList()
+    skip.add(5)
+    skip.printList()
+    skip.add(6)
+    skip.printList()
+    skip.add(3)
+    skip.printList()
+    skip.add(2)
+    skip.printList()
